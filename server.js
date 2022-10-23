c14bc363d1ffed73184503310f4bb9976ad8273d
Cody Christian <codychristian@Codys-Air.attlocal.net>
2022-10-22 20:54:23 -0500

    configured middleware and post endpoint

--- server.js
+++ server.js
@@ -7,11 +7,19 @@ const app = express();
 //Connect Database
 connectDatabase();
 
-//API endpoint
+//Middleware
+app.use(express.json({ extended: false }));
+
+//API endpoints
 
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
+
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 //Connection Listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
